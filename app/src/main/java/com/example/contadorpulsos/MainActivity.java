36a8086fd48d260205e4fb39c85fe426f7b82c8d
Iker <llorenteiker99@gmail.com>
2023-02-22 20:48:53 +0100

    Calculadora v1.0.0

--- app/src/main/java/com/example/contadorpulsos/MainActivity.java
+++ app/src/main/java/com/example/contadorpulsos/MainActivity.java
@@ -1,39 +1,62 @@
 package com.example.contadorpulsos;
 
+import static java.lang.Integer.parseInt;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     // Propiedades de la actividad
-    public int contadorNum;
+    public String num1 = "";
+    public String aux = "";
+    public String operacion = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        contadorNum = 0;
-
     }
 
-    public void aumentarContador(View boton){
-        TextView contadorVista = (TextView)findViewById(R.id.contador);
-        contadorNum++;
-        contadorVista.setText("Contador: " + contadorNum);
+    public void concatNum(View boton){
+        Button botonDisp = (Button)boton;
+        this.num1 += botonDisp.getText();
+        TextView res = (TextView)findViewById(R.id.res);
+        res.setText(this.num1);
     }
 
-    public void resetearContador(View boton){
-        TextView contadorVista = (TextView)findViewById(R.id.contador);
-        contadorNum = 0;
-        contadorVista.setText("Contador: " + contadorNum);
+    public void allocateOperation(View boton){
+        Button botonDisp = (Button)boton;
+        this.operacion = botonDisp.getText().toString();
+        this.aux = this.num1;
+        this.num1 = "";
+        TextView res = (TextView)findViewById(R.id.res);
+        res.setText("");
     }
 
-    public void decrementarContador(View boton){
-        TextView contadorVista = (TextView)findViewById(R.id.contador);
-        contadorNum--;
-        contadorVista.setText("Contador: " + contadorNum);
+    public void resolveOperation(View boton){
+        TextView res = (TextView)findViewById(R.id.res);
+        int resNum = 0;
+        switch (this.operacion){
+            case "+":
+                resNum = parseInt(this.aux) + parseInt(this.num1);
+                break;
+            case "-":
+                resNum = parseInt(this.aux) - parseInt(this.num1);
+                break;
+            case "*":
+                resNum = parseInt(this.aux) * parseInt(this.num1);
+                break;
+            case "/":
+                resNum = parseInt(this.aux) / parseInt(this.num1);
+                break;
+        }
+        res.setText(String.valueOf(resNum));
+        this.num1 = res.getText().toString();
     }
+
+
 
 }
